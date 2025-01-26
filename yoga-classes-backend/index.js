57dbaecbe2e661d62f8822bda442493c17e96e49
adarshmanwal <adarshmanwal9@gmail.com>
2025-01-26 22:49:26 +0530

    intial commit fornt end and backend added

---
+++ yoga-classes-backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const { Sequelize } = require("sequelize");
+const port = process.env.PORT
+require('dotenv').config();
+const config = require("./src/config/config");
+// const routes = require("./routes");
+const bodyParser = require('body-parser');
+var cors = require('cors')
+
+app.use(bodyParser.json({limit:'50mb'})); 
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'})); 
+
+// Test the database connection
+const sequelize = new Sequelize(config.development);
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected."))
+  .catch((err) => console.error("Unable to connect to the database:", err));
+
+// const userRoutes = require("./routes/userRoutes");
+// app.use("/users", userRoutes);
+
+// const shopRoutes = require("./routes/shopRoutes");
+// app.use("/shops", shopRoutes);
+
+// app.use(userRoutes);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
