93b14059adf6c32b7613cbe590df8ce7999031d6
adarshmanwal <adarshmanwal9@gmail.com>
2025-01-27 11:37:43 +0530

    active class added

--- yoga-classes-front-end/src/components/Header.jsx
+++ yoga-classes-front-end/src/components/Header.jsx
@@ -1,8 +1,6 @@
-import { NavLink, useRouteLoaderData } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-  const token = useRouteLoaderData("root");
-
   return (
     <nav className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -11,15 +9,23 @@ function Header() {
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 <NavLink
-                to="/"
-                  className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "rounded-md px-3 py-2 text-sm font-medium bg-gray-700 text-white"
+                      : "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                  }
                   aria-current="page"
                 >
                   Home
                 </NavLink>
                 <NavLink
-                    to="auth?mode=login"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                  to="auth?mode=login"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "rounded-md px-3 py-2 text-sm font-medium bg-gray-700 text-white"
+                      : "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                  }
                 >
                   Authenticate
                 </NavLink>
