9b3fa936efe8cde7e1e5b2671477b7413d83e472
adarshmanwal <adarshmanwal9@gmail.com>
2025-01-30 21:49:23 +0530

    SideNav added

--- yoga-classes-front-end/src/pages/Root.jsx
+++ yoga-classes-front-end/src/pages/Root.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import SideNav from "../components/SideNav";
 
 export default function Root() {
   return (
     <>
       <Header></Header>
-      <main>
-        <Outlet></Outlet>
-      </main>
+      <div className="flex flex-col md:flex-row space-x-1">
+        <div className="">
+          <SideNav></SideNav>
+        </div>
+        <div className="flex-1">
+          <main>
+            <Outlet></Outlet>
+          </main>
+        </div>
+      </div>
     </>
   );
 }
