aea49a1e4b0f39a9c022c44fefb2d93d44ad8fec
adarshmanwal <adarshmanwal9@gmail.com>
2025-06-23 14:07:38 +0530

    dynamic sidebar

--- yoga-classes-front-end/src/components/Header.jsx
+++ yoga-classes-front-end/src/components/Header.jsx
@@ -1,10 +1,27 @@
 import { useContext, useState } from "react";
 import { NavLink, useRouteLoaderData, Form } from "react-router-dom";
 import { UserContext } from "../store/user/user-context";
+import { AiOutlineMenu } from "react-icons/ai";
+import { GlobalContext } from "../store/global-context";
 
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+    <button
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+      className="relative text-x1 rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right top-2"
+      />
+      {icon}
+    </button>
+);
 function Header() {
   const token = useRouteLoaderData("root");
   const userData = useContext(UserContext);
+  const {activeMenue, setActiveMenu} = useContext(GlobalContext);
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
@@ -12,17 +29,12 @@ function Header() {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           {/* Home Link */}
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? "rounded-md px-3 py-2 text-sm font-medium bg-gray-700 text-white"
-                : "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-            }
-            aria-current="page"
-          >
-            Home
-          </NavLink>
+          <NavButton
+            title="menu"
+            customFunc={() => setActiveMenu((prev) => !prev)}
+            color="white"
+            icon={<AiOutlineMenu></AiOutlineMenu>}
+          ></NavButton>
 
           {/* Right Section */}
           {token ? (
@@ -30,7 +42,7 @@ function Header() {
               <span className="text-sm font-medium text-gray-300">
                 {userData.user.email}
               </span>
-              
+
               {/* Profile Dropdown */}
               <div className="relative">
                 <button
@@ -44,7 +56,7 @@ function Header() {
                     alt="User Avatar"
                   />
                 </button>
-                
+
                 {/* Dropdown Menu */}
                 {menuOpen && (
                   <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 ring-1 ring-black/5 focus:outline-none z-50">
---
+++ yoga-classes-front-end/src/store/global-context.jsx
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+
+export const GlobalContext = createContext();
+
+export default function GlobalContextProvider({ children }) {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const ctx = {
+    activeMenu,
+    setActiveMenu
+  };
+  return <GlobalContext.Provider value={ctx}>{children}</GlobalContext.Provider>;
+}
